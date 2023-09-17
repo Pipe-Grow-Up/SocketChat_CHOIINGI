6256c9667e984bc27b79024873ff5fd9fccce98b
haerimRoh <mtxpvksh@gmail.com>
2023-09-17 17:14:20 +0900

    refacor. scanner bufferreader로 대체

--- src/server/ServerNoticeThread.java
+++ src/server/ServerNoticeThread.java
@@ -1,27 +1,26 @@
 package server;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Map;
-import java.util.Scanner;
 
 import static Global.global.clients;
 
 public class ServerNoticeThread extends Thread {
     // 서버 공지를 위한 글 전체 공지 메세지 전송
     // 네이밍은 쓰레드를 상속 받을 시, 클래스에 Thread를 적어주는 것이 좋을듯.
-
-    Scanner scanner;
-
+    BufferedReader mReader; // BufferReader 변수 reader 선언
 
     public void run() {
 
-        scanner = new Scanner(System.in);
-        // 소켓에 대한 송신 준비
+        mReader = new BufferedReader(new BufferedReader(new InputStreamReader(System.in))); // 사용자의 입력을 받으
+
         try {
             while (true) {
                 // 전할 메세지 입력
-                String message = scanner.nextLine();
+                String message = mReader.readLine();
                 if (message.equals("!userList")) {
                     // 접속 사용자 리스트 체크
                     System.out.println("연결된 유저 목록");
@@ -35,10 +34,9 @@ public class ServerNoticeThread extends Thread {
                     for (Map.Entry<Socket, String> entry : clients.entrySet()) {
                         //계정 관리
                         PrintWriter writer = new PrintWriter(entry.getKey().getOutputStream(), true);
-                        writer.println(msg);
+                        writer.println("From server to All: "+msg);
                     }
-                }
-                else{
+                } else {
 
                 }
             }
